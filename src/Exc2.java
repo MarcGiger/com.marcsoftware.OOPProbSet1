c253a393425f6d9c8530a0eaabbfc78cd258e042
t00210159 <does@care.com>
2019-09-17 18:00:43 +0100

    Initial commit

---
+++ src/Exc2.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Exc2 {
+
+    public static void main(String[] args) {
+
+        Integer n = Integer.parseInt(.showInputDialog(null, "enter how many Times it should repeat"));
+
+        for(int i=1;i<=n;i++) {
+
+JTextArea
+
+        }
+
+
+    }
+}
