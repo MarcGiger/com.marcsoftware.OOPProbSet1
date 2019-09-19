d948d0fdfbf7e8903376163618d3800b79394df1
t00210159 <Marc.Giger@students.ittralee.ie>
2019-09-19 16:00:28 +0100

    exc5+6+7+8

---
+++ src/Exc8.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Exc8 {
+    public static void main(String[] args) {
+
+        int[] numbers = new int[10];
+
+
+
+        for(int a=0;a<=9;a++){
+
+          numbers[a] = Integer.parseInt(JOptionPane.showInputDialog(null,"Please enter an integer","do it",-1));
+
+        }
+        System.out.println(Arrays.toString(numbers));
+
+
+    }
+}
